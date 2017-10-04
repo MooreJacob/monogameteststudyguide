e4adce95ff00202a292476b6bff3fb31a044e651
pwd <pwd@bppwd.com>
2017-10-04 14:04:44 -0400

    Jake did something

--- WorkFlow/Enemy.cs
+++ WorkFlow/Enemy.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace WorkFlow
 {
-    class Enemy
+    public class Enemy
     {
+        private Texture2D solidTexture;
+        private Rectangle box;
+        private int x;
+        private int y;
+    public Enemy(int a, int b)
+        {
+            int x = a;
+            int y = b;
+        }
+     public void LoadContent(GraphicsDevice graphics)
+        {
+            solidTexture = new Texture2D(graphics, 1, 1);
+            solidTexture.SetData(new Color[] { Color.White });
+            box = new Rectangle(x, y, 50, 50);
+        }
+
+    public void Initialize()
+        {
+            Initialize();
+        }
     }
 }
