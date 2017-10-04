18d2505f10000dfebaf839fdbfd9a65b3bdeaccd
Robert <R.ruiz@baypath.netgit config -- global user.name Robert/..gti config --global user.name Robert>
2017-10-04 09:57:23 -0400

    Player methods added

--- WorkFlow/Player1.cs
+++ WorkFlow/Player1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,31 @@ namespace WorkFlow
 {
     class Player1
     {
+        private Texture2D solidTexture;
+        private GraphicsDevice spriteBatch;
+        private Rectangle p;
+
+        public void Loadcontent()
+        {
+            solidTexture = new Texture2D(spriteBatch, 1, 1);
+            
+            solidTexture.SetData(new Color[] { Color.White });
+            p = new Rectangle(50, 100, 250, 250);
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+           
+
+        }
+
+        public void update()
+        {
+
+     
+
+        }
+        
     }
+
 }
