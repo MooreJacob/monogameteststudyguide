b1bf160b8aa4b93a34f8e4ab78a1b0b3cc32e86a
Robert <R.ruiz@baypath.netgit config -- global user.name Robert/..gti config --global user.name Robert>
2017-10-04 10:15:38 -0400

    Player movement Added

--- WorkFlow/Player1.cs
+++ WorkFlow/Player1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,28 +14,53 @@ namespace WorkFlow
         private Texture2D solidTexture;
         private GraphicsDevice spriteBatch;
         private Rectangle p;
+        private int y;
+        private int x;
 
         public void Loadcontent()
         {
             solidTexture = new Texture2D(spriteBatch, 1, 1);
             
             solidTexture.SetData(new Color[] { Color.White });
-            p = new Rectangle(50, 100, 250, 250);
+            p = new Rectangle(x, y , 150, 150);
         }
 
-        public void Draw(SpriteBatch sb)
-        {
-           
-
-        }
-
+    
         public void update()
         {
+            KeyboardState state = Keyboard.GetState();
 
-     
+            bool leftArrowKeyDown = state.IsKeyDown(Keys.Left);
+
+            if (state.IsKeyDown(Keys.Left))
+            {
+                x -= 5;
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                x += 5;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                y -= 5;
+            }
+            if (state.IsKeyDown(Keys.Down))
+            {
+                y += 5;
+            }
+        }
+
+        public void Draw(SpriteBatch sb,GameTime gt)
+        {
+            sb.Begin();
+
+            Draw(sb, gt);
+
+            sb.End();
 
         }
-        
+
+
     }
 
 }
